be2b7bb21928d1f5dafa685c8e14ef2e50226397
Marina <78799773+marinezh@users.noreply.github.com>
2023-01-25 11:19:59 +0200

    first commit

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("PMApp in TS begins...");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.querySelector("#project-input");
+        this.hostElement = document.querySelector("#app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
